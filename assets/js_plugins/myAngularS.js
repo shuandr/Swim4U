80bf8f38fc5282c40fb064ba67578cb78bdbc87e
shuandr <shuandr@gmail.com>
2021-03-02 18:06:35 +0200

    ver 2.0.0

--- assets/js_plugins/myAngularS.js
+++ assets/js_plugins/myAngularS.js
@@ -1,20 +1,29 @@
-var app = angular.module('sport4U', ['ngRoute', 'ngSanitize']);
-
-
+var app = angular.module('sport4U', ['ngRoute', 'ngLocale', 'ngSanitize', 'ngAnimate', 'slickCarousel']);
 
 app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{[{');
     $interpolateProvider.endSymbol('}]}');
 });
 
+app.controller('sportCtrl', function($scope, $http, $timeout) {
 
-
-
-app.controller('sportCtrl', function($scope, $http) {
-
+    $scope.dataLoaded = true;
 
     $http.get("assets/json/data-S.json").then(function(response) {
         $scope.data = response.data;
-
+        $scope.dataLoaded = false;
+        $timeout(function() {
+            $scope.dataLoaded = true;
+        }, 20);
     });
+
+    $scope.mainSlickConfig = {
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        autoplaySpeed: 1800,
+        speed: 600,
+        centerMode: true,
+        variableWidth: true,
+    };
 });
